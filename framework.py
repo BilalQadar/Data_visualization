58e002e0a5ccd85926aa4705cc55fe5fd3063d79
Bilal Qadar <qadarbilal@gmail.com>
2019-08-21 14:49:43 -0400

    Linear and polynomial trendline feature

--- framework.py
+++ framework.py
@@ -16,7 +16,11 @@ class DataVis:
         self.x_label = ""
         self.y_label = ""
         self.title = title
+        self.caption = ""
         plot = None
+
+    def add_caption(self,caption):
+        self.caption = caption
 
     def add_data(self,x,y):
         if type(x[0]) == str:
@@ -46,7 +50,7 @@ class DataVis:
     def update_title(self,new_title):
         self.title = new_title
 
-    def get_trendline(self):
+    def calc_trendline(self,degree):
         if type(self.x[0]) == str:
             size = range(0,len(self.x))
             x = []
@@ -58,7 +62,7 @@ class DataVis:
             x = self.x[:, np.newaxis]
         y = self.y[:, np.newaxis]
 
-        polynomial_features= PolynomialFeatures(degree=3)
+        polynomial_features= PolynomialFeatures(degree=degree)
         x_poly = polynomial_features.fit_transform(x)
 
         model = LinearRegression()
@@ -66,22 +70,37 @@ class DataVis:
         y_poly_pred = model.predict(x_poly)
         self.trendline = y_poly_pred
 
-    def plot(self,parameters,trendline,grid,destination_folder):
-        (data_fmt, trendline_fmt, line_width) = parameters
+    def plot(self,skip_data,parameters,trendline,grid,destination_folder):
+        (data_color, data_line, trend_color, trend_line, line_width) = parameters
+
+        plt.figure(figsize=(12, 9))
+        ax = plt.subplot(111)
+        ax.get_xaxis().tick_bottom()
+        ax.get_yaxis().tick_left()
+
         if self.x[0] is False or self.y[0] is False:
             raise TypeError("Add data using add_data(x,y)")
 
         else:
-            plt.plot(self.x,self.y, data_fmt)
+            plt.plot(self.x[skip_data:],self.y[skip_data:], color=data_color, linestyle=data_line,linewidth=line_width)
 
-        if trendline != False:
-            self.get_trendline()
-            plt.plot(self.x, self.trendline, trendline_fmt)
+        if trendline != 0:
+            print(trendline)
+            self.calc_trendline(trendline)
+            plt.plot(self.x[skip_data:], self.trendline[skip_data:], color=trend_color, linestyle=trend_line, linewidth=line_width)
 
-        plt.title(self.title, fontweight="bold",fontname="Open Sans")
-        plt.xlabel(self.x_label,fontname="Open Sans", fontweight="bold")
-        plt.ylabel(self.y_label,fontname="Open Sans", fontweight="bold")
-        plt.xticks([])
+        plt.title(self.title, fontsize=14,fontname="Open Sans")
+        plt.xlabel(self.x_label,fontsize=14, fontname="Open Sans")
+        plt.ylabel(self.y_label,fontsize=14,fontname="Open Sans")
+        #Uncomment if want to specify  xtick labels and positions
+        ## TODO: Make this more robust
+        #[2006,2009,2012,2015,2019]
+        # plt.xticks([0,(len(self.x) - skip_data)/4,(len(self.x) - skip_data)/2, (len(self.x) - skip_data) - (len(self.x) - skip_data)/4,len(self.x) - skip_data],[2006,2009,2012,2015,2019])
         if grid is True:
             plt.grid(True)
+
+        if self.caption != "":
+            plt.figtext(0.09, 0.01, self.caption, wrap=True, horizontalalignment='left', fontsize=10)
+
+        bbox_inches="tight"
         plt.savefig(os.path.join(destination_folder,self.title) + ".png" )
--- main.py
+++ main.py
@@ -2,37 +2,50 @@ from framework import *
 import numpy as np
 
 if __name__ == "__main__":
-    parameters = ("#de312a","k--", 20)
-    destination_folder = "/Users/bilalqadar/Documents/data_visualization/saved_files"
-    #
+    parameters = ("#763dff",'-',"#000000",'--', 3)
+    destination_folder = "/Users/bilalqadar/Documents/GitHub/Data_visualization/saved_figures/"
+
     # x_bully = [2007,2009,2010,2011,2013,2014,2015,2016,2019]
     # y_bully = [18.8,21.5,20.8,29.2,24.1,34.6,34.0,33.6,36.5]
-    # bullying_title = "Cyberbullying Victimization Rates"
+    # bullying_title = "Cyberbullying Victimization Rates (2007-2019)"
     # bullying_plot = DataVis(bullying_title)
+    # bullying_plot.add_caption("Figure 1: Percentage of Toronto students who identify with being bullied each year. Dataset is of random Toronto middle and highschool students (N=3000).")
     # bullying_plot.add_data(x_bully,y_bully)
     # bullying_plot.add_label("x","Year")
     # bullying_plot.add_label("y", "Bullying Rate (%)")
-    # bullying_plot.plot(parameters,True,True,destination_folder)
-    #
+    # bullying_plot.plot(0,parameters,3,False,destination_folder)
+
+    # cyber_title = "Cyberbullying Related Google Searches (2004-2019)"
+    # file_location ="/Users/bilalqadar/Documents/GitHub/Data_visualization/csv_files/cyberbullying.csv"
+    # cyber_trend = DataVis(cyber_title)
+    # cyber_trend.add_caption("Figure 2: Candian Google searches related to cyberbullying since 2004. Youth sometimes don’t feel comfortable identifying being bullied. The data is normalized with 100 being the peak number of searches and all other data points scaled appropriately.")
+    # cyber_trend.add_data_csv(file_location, 2, "Month", "Cyberbullying")
+    # cyber_trend.add_label("y", "Normalized Interest")
+    # cyber_trend.plot(0,parameters,3,False,destination_folder)
+
+    insta_title = "Instagram Bullying Related Google Searches (2006-2019)"
+    file_location ="/Users/bilalqadar/Documents/GitHub/Data_visualization/csv_files/instaBully.csv"
+    insta_trend = DataVis(insta_title)
+    insta_trend.add_caption("Figure 3: Canadian Google searches related to instagram bullying since 2006. The data is normalized with 100 being the peak number of searches and all other data points scaled appropriately.")
+    insta_trend.add_data_csv(file_location, 2, "Month", "instagram bullying: (Worldwide)")
+    insta_trend.add_label("y", "Normalized Interest (%)")
+    insta_trend.add_label("x","Time (Years)")
+    insta_trend.plot(48,parameters,3,False,destination_folder)
+
     # y_vc = [1147.0,2613.0,3297.0,4093.0,5425.0,9334.0]
     # x_vc = [2013,2014,2015,2016,2017,2018]
-    # vc_title = "Total VC funding for AI & ML startups"
+    # vc_title = "Total VC funding for AI & ML startups (2013-2018)"
     # vc_plot = DataVis(vc_title)
+    # vc_plot.add_caption("Figure 4: Amount of VC funding for AI & Machine learning startups over time. As trendline suggest VC's investing in machine learning is growing linearly with respect to time.")
     # vc_plot.add_data(x_vc,y_vc)
     # vc_plot.add_label("x","Year")
     # vc_plot.add_label("y", "VC funding (Millions)")
-    # vc_plot.plot(parameters,False,True,destination_folder)
-    #
-    # cyber_title = "Cyberbullying Related Google Searches"
-    # file_location ="/Users/bilalqadar/Documents/data_visualization/csv_files/multiTimeline.csv"
-    # cyber_trend = DataVis(cyber_title)
-    # cyber_trend.add_data_csv(file_location, 2, "Month", "Cyberbullying")
-    # cyber_trend.add_label("y", "Normalized Interest")
-    # cyber_trend.plot(parameters,True,False,destination_folder)
+    # vc_plot.plot(0,parameters,1,False,destination_folder)
 
-    cyber_title = "Cyberbullying Related Google Searches"
-    file_location ="/Users/bilalqadar/Documents/data_visualization/csv_files/multiTimeline.csv"
-    cyber_trend = DataVis(cyber_title)
-    cyber_trend.add_data_csv(file_location, 2, "Month", "Cyberbullying")
-    cyber_trend.add_label("y", "Normalized Interest")
-    cyber_trend.plot(parameters,True,False,destination_folder)
+    # howML_title = "How to Learn Machine Learning Related Google Searches (2004-2019)"
+    # file_location ="/Users/bilalqadar/Documents/GitHub/Data_visualization/csv_files/howML.csv"
+    # howML = DataVis(howML_title)
+    # howML.add_caption("Figure 5: anadian Google searches related to learning about machine learning since 2006. The data is normalized with 100 being the peak number of searches and all other data points scaled appropriately.")
+    # howML.add_data_csv(file_location, 2, "Month", "how to learn machine learning: (Worldwide)")
+    # howML.add_label("y", "Normalized Interest")
+    # howML.plot(0,parameters,3,False,destination_folder)
